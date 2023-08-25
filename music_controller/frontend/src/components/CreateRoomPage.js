0c408bc9be52ad7bddc95eb00dc1ea5cce26d3dc
Ethan S <es5888@nyu.edu>
2023-08-25 15:56:48 -0400

    Need to fix server 500 error for Create a Room

--- music_controller/frontend/src/components/CreateRoomPage.js
+++ music_controller/frontend/src/components/CreateRoomPage.js
@@ -5,32 +5,121 @@ import Typography from "@material-ui/core/Typography";
 import TextField from '@material-ui/core/TextField';
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
-import  { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { ThemeProvider, createTheme } from '@material-ui/core/styles';
 
 export default class CreateRoomPage extends Component {
     constructor(props) {
         super(props);
+        this.defaultVotes = 2;
+        this.state = {
+            guestCanPause: true,
+            votesToSkip: this.defaultVotes,
+        };
+        this.handleRoomButtonPressed = this.handleRoomButtonPressed.bind(this);
+        this.handleGuestCanPauseChange = this.handleGuestCanPauseChange.bind(this);
+        this.handleVotesChange = this.handleVotesChange.bind(this);
+    }
+
+    handleVotesChange(e) {
+        this.setState({
+            votesToSkip: e.target.value,
+        });
+    }
+
+    handleGuestCanPauseChange(e) {
+        this.setState({
+            guestCanPause: e.target.value === 'true' ? true : false,
+        });
+    }
+
+    handleRoomButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                votes_to_skip: this.state.votesToSkip,
+                guest_can_pause: this.state.guestCanPause,
+            }),
+        };
+        fetch("/api/create-room", requestOptions)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then((data) => console.log(data))
+            .catch((error) => console.error("There was a problem with the fetch operation:", error));
     }
 
     render() {
         return (
-            <Grid container spacing={1}>
-                <Grid item xs={12} align="center">
-                    <Typography component="h4" variant="h4">
-                        Create a Room 
-                    </Typography> 
-                    <FormControl component="fieldset"> 
-                        <FormHelperText>
-                            <div align="center">
-                                Guest Control of Playback State
-                            </div>
-                        </FormHelperText>
-                    </FormControl>
+            <ThemeProvider theme={createTheme()}>
+                <Grid container spacing={1}>
+                    <Grid item xs={12} align="center">
+                        <Typography component="h4" variant="h4">
+                            Create a Room
+                        </Typography>
+                        <FormControl component="fieldset">
+                            <FormHelperText>
+                                <div align="center">
+                                    Guest Control of Playback State
+                                </div>
+                            </FormHelperText>
+                            <RadioGroup row defaultValue="true" onChange={this.handleGuestCanPauseChange}>
+                                <FormControlLabel
+                                    value="true"
+                                    control={<Radio color="primary" />}
+                                    label="Play/Pause"
+                                    labelPlacement="bottom"
+                                />
+                                <FormControlLabel
+                                    value="false"
+                                    control={<Radio color="secondary" />}
+                                    label="No Control"
+                                    labelPlacement="bottom"
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12} align="center">
+                        <FormControl>
+                            <TextField 
+                                required={true}
+                                type="number"
+                                onChange={this.handleVotesChange}
+                                defaultValue={this.defaultVotes}
+                                inputProps={{
+                                    min: 1,
+                                    style: { textAlign: "center" }
+                                }}
+                            />
+                            <FormHelperText>
+                                <div align="center">
+                                    Votes Required to Skip Song
+                                </div>
+                            </FormHelperText>
+                        </FormControl>
+                    </Grid>
+
+                    <Grid item xs={12} align="center">
+                        <Button color="primary" variant="contained" onClick={this.handleRoomButtonPressed} style={{ marginBottom: '-20px' }}>
+                            Create A Room
+                        </Button>
+                    </Grid>
+
+                    <Grid item xs={12} align="center">
+                        <Button color="secondary" variant="contained" to="/" component={Link}>
+                            Back
+                        </Button>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </ThemeProvider>
         );
     }
-} 
+}
